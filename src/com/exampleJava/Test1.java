16c943ca940a777ce6a376ace615bb5e46307d66
SumanKusa <sumankusa1998@gmail.com>
2023-01-17 19:05:01 +0530

    This is my Java Project

---
+++ src/com/exampleJava/Test1.java
@@ -0,0 +1,13 @@
+package com.exampleJava;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		int b=12;
+	System.out.println(b);
+
+	}
+
+}
